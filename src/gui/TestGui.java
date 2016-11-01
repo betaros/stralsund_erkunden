d39a13643a97906f9a2c3effde723cdd86adc4b5
Tobias Schulz <tobias@rapidrabbit.com>
2016-11-01 21:35:11 +0100

    Test

--- src/gui/TestGui.java
+++ src/gui/TestGui.java
@@ -1,6 +1,10 @@
 package gui;
 
 import javax.swing.JFrame;
+import javax.swing.JButton;
+import java.awt.BorderLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class TestGui {
 
@@ -20,6 +24,14 @@ public class TestGui {
 		testFrame = new JFrame();
 		testFrame.setBounds(100, 100, 450, 300);
 		testFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JButton btnNewButton = new JButton("New button");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		testFrame.getContentPane().add(btnNewButton, BorderLayout.WEST);
 	}
 
 }
